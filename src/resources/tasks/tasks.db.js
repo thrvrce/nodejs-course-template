3d3b1d41b81a1a19119965e6597b7910e1f52296
Victor Avdeev <avdeevvictor95@gmail.com>
2021-05-16 21:49:31 +0300

    feat: getall, getbyid, update, delete task routes

---
+++ src/resources/tasks/tasks.db.js
@@ -0,0 +1,43 @@
+const uuid = require('uuid').v4;
+
+
+const tasksDB = [
+  {
+    id : '1',
+    title : 'title_1',
+    order : 0,
+    description : 'description_1',
+    userId : '1',
+    boardId : '1',
+    columnId : '1',
+  },
+  {
+    id : 'undefined',
+    title : 'title_1',
+    order : 0,
+    description : 'description_1',
+    userId : '1',
+    boardId : 'undefined',
+    columnId : '1',
+  },
+  {
+    id : '2',
+    title : 'title_2',
+    order : 0,
+    description : 'description_2',
+    userId : '2',
+    boardId : '2',
+    columnId : '1',
+  },
+  {
+    id : uuid(),
+    title : 'title_3',
+    order : 1,
+    description : 'description_3',
+    userId : '1',
+    boardId : '1',
+    columnId : '1',
+  },
+]
+
+module.exports = tasksDB;
---
+++ src/resources/tasks/tasks.memory.repository.js
@@ -0,0 +1,33 @@
+const tasksDB = require('./tasks.db');
+const Task = require('./tasks.model');
+
+const getAll = async (boardId) => boardId && boardId !== 'undefined' ? tasksDB.filter(task =>  task.boardId === boardId) : tasksDB;
+const getById = async (taskId) => tasksDB.filter((task) => task.id === taskId)
+const createtask = async (taskData) => {
+  const newtask = new Task(taskData);
+  tasksDB.push(newtask);
+  return newtask;
+}
+const updatetask = async (taskId, taskData) => {
+  const [task] = await getById(taskId);
+  if (task) {
+    Object.keys(taskData).forEach(property => {task[property] = taskData[property]});
+  }
+
+  return task;
+}
+
+const deletetask = async (boardId, taskId) => {
+  const [task] = await getById(taskId);
+
+  const taskIndex = tasksDB.indexOf(task);
+
+  let deletedtask;
+  if (taskIndex !== -1) {
+    [deletedtask] = tasksDB.splice(taskIndex, 1);
+  }
+
+  return deletedtask;
+}
+
+module.exports = { getAll, getById, createtask, updatetask, deletetask };
