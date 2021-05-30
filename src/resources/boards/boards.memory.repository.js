3f207e7e43adbb1ac8553a961567270cc62e3f49
Victor Avdeev <avdeevvictor95@gmail.com>
2021-05-30 23:22:32 +0300

    feat: jsdoc for boards

--- src/resources/boards/boards.memory.repository.js
+++ src/resources/boards/boards.memory.repository.js
@@ -1,14 +1,37 @@
 const boardsDB = require('./boards.db');
 const Board = require('./boards.model');
 const taskService = require('../tasks/tasks.service');
-
+/**
+ * Returns array of all boards or by board.id if it specified
+ * @param {string | undefined} boardId board.id or undefined
+ * @returns {array} array of all boards or boads with specific id
+ */
 const getAll = async (boardId) => boardId && boardId !== 'undefined' ? boardsDB.filter(board =>  board.id === boardId) : boardsDB;
+
+/**
+ * Returns board by id
+ * @param {string} boardId board.id property
+ * @returns {object} board object
+ */
 const getById = async (boardId) => boardsDB.filter((board) => board.id === boardId)
+
+/**
+ * Creates new board object
+ * @param {object} boardData object with board properties
+ * @returns {object} board object
+ */
 const createboard = async (boardData) => {
   const newboard = new Board(boardData);
   boardsDB.push(newboard);
   return newboard;
 }
+
+/**
+ * Updates board by id
+ * @param {string} boardId board.id property
+ * @param {object} boardData object with board new propertt values
+ * @returns {object | undefined} updated board object
+ */
 const updateboard = async (boardId, boardData) => {
   const [board] = await getById(boardId);
   if (board) {
@@ -18,6 +41,11 @@ const updateboard = async (boardId, boardData) => {
   return board;
 }
 
+/**
+ * Deletes board by id
+ * @param {string} boardId board.id property
+ * @returns {object | undefined} deleted board object
+ */
 const deleteboard = async (boardId) => {
   const [board] = await getById(boardId);
 
--- src/resources/boards/boards.model.js
+++ src/resources/boards/boards.model.js
@@ -1,6 +1,12 @@
 const uuid = require('uuid').v4;
 
 class Board {
+  /**
+ * Board object constructor
+ * @param {string} title board title
+ * @param {array} columns board columns array
+ * @returns {object} new Board object
+ */
   constructor({
     id = uuid(),
     title,
@@ -11,6 +17,11 @@ class Board {
     this.columns = columns;
   }
 
+  /**
+   *
+   * @param {object} board board object
+   * @returns {object} board object
+   */
   static toResponse(board) {
 
 
